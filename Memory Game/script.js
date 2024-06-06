b7813b10a21aaa558229ed924a4993b500f35f7d
steefanb <stefanb2001b@gmail.com>
2024-06-07 00:53:25 +0200

    Dodati projektni fajlovi za igru pamcenja

---
+++ Memory Game/script.js
@@ -0,0 +1,63 @@
+const cards = document.querySelectorAll('.memory-card');
+
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
+function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
+    this.classList.toggle('flip');
+
+    if (!hasFlippedCard) {
+        // prvi klik
+        hasFlippedCard = true;
+        firstCard = this;
+
+        return;
+    } 
+        // drugi klik
+        secondCard = this;
+
+        checkForMatch();
+
+}
+
+function checkForMatch() {
+let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+
+isMatch ? disableCards() : unflipCards();
+}
+
+function disableCards() {
+    firstCard.removeEventListener('click', flipCard);
+    secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
+}
+
+function unflipCards() {
+    lockBoard = true;
+// neuspesno
+setTimeout(() => { 
+    firstCard.classList.remove('flip');
+    secondCard.classList.remove('flip');
+
+    resetBoard();
+    }, 1500);
+}
+
+function resetBoard () {
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
+}
+
+(function shuffle() {
+    cards.forEach(card => {
+        let randomPos = Math.floor(Math.random() * 12);
+        card.style.order = randomPos;
+    });
+})();
+
+cards.forEach(card => card.addEventListener('click', flipCard));
